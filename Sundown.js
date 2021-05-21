b3aacf67f0d8ec89f126fe826ba84f367a6257b0
Deji14 <djsoft2001@gmail.com>
2021-05-21 13:56:32 -0700

    refix

--- Sundown.js
+++ Sundown.js
@@ -1,31 +1,6 @@
    
    $(document).ready(function()
    {
-      $('#wb_Text2').addClass('visibility-hidden');
-      function onScrollText2()
-      {
-         var $obj = $("#wb_Text2");
-         if (!$obj.hasClass("in-viewport") && $obj.inViewPort(false))
-         {
-            $obj.addClass("in-viewport");
-            AnimateCss('wb_Text2', 'transform-lightspeed-in', 150, 1000);
-         }
-         else
-         if ($obj.hasClass("in-viewport") && !$obj.inViewPort(true))
-         {
-            $obj.removeClass("in-viewport");
-            AnimateCss('wb_Text2', 'animate-fade-out', 0, 0);
-         }
-      }
-      if (!$('#wb_Text2').inViewPort(true))
-      {
-         $('#wb_Text2').addClass("in-viewport");
-      }
-      onScrollText2();
-      $(window).scroll(function(event)
-      {
-         onScrollText2();
-      });
       function skrollrInit()
       {
          skrollr.init({forceHeight: false, mobileCheck: function() { return false; }, smoothScrolling: false});
