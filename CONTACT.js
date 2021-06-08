8b02a8b1ad85cfd8f7975e56ce4bc81b24fb89be
Deji14 <djsoft2001@gmail.com>
2021-06-08 09:59:19 -0700

    RWD

--- CONTACT.js
+++ CONTACT.js
@@ -1,6 +1,7 @@
    
    $(document).ready(function()
    {
+      $("#Layer1").stickylayer({orientation: 1, position: [0, 0], delay: 500});
       var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
       if (iOS)
       {
